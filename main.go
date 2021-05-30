fb093a2f5e13359c1b30641689ec50ec7c80656b
S3cBar0n <secberg@gmail.com>
2021-05-30 13:31:26 -0600

    Added smtp library to enable sending email reports when an IP is detected as down.

--- main.go
+++ main.go
@@ -5,7 +5,9 @@ import (
 	"fmt"
 	"io"
 	"log"
+	"net/smtp"
 	"os"
+	"strings"
 
 	"github.com/go-ping/ping"
 )
@@ -16,12 +18,11 @@ func netWorker(host string, receive chan string) {
 		fmt.Println("Failed to launch the pinger service.")
 		return
 	}
-	pinger.SetPrivileged(true)
 	pinger.Timeout = 10000000 * 1000
 	pinger.OnFinish = func(stats *ping.Statistics) {
 		//fmt.Printf("%d packets transmitted, %d packets received, %d duplicates, %v packet loss\n",
 		//	stats.PacketsSent, stats.PacketsRecv, stats.PacketsRecvDuplicates, stats.PacketLoss)
-		if stats.PacketLoss >= 50.0 {
+		if stats.PacketLoss >= 35.0 {
 			host = fmt.Sprintf("%s - %s with %v", "Offline", host, stats.AvgRtt)
 			receive <- host
 			return
@@ -36,9 +37,35 @@ func netWorker(host string, receive chan string) {
 	}
 }
 
+func emailSend(offlineHosts []string) {
+	fmt.Println(offlineHosts)
+	authentication := smtp.PlainAuth("", "USERNAME", "SECURELY STORED PASSWORD", "MAILSERVER WITHOUT PORT")
+
+	to := []string{"ADDRESS TO SEND THE MAIL TO"}
+	msg := []byte(
+		"To: ADDRESS TO SEND THE MAIL TO\r\n" +
+			"Subject: Network Down\r\n" +
+			"MIME-version: 1.0;\nContent-Type: text/html;\r\n" +
+			"\r\n" +
+			"<p>The <strong>following</strong> IP's have gone offline:\r\n")
+
+	for item := range offlineHosts {
+		item_msg := fmt.Sprintf("<p>%s</p>", offlineHosts[item])
+		msg = append(msg, item_msg...)
+	}
+	msg = append(msg, "<p>Report any strange behavior to the Network Administrator.<br><br>Thanks,<br>NetworkMonitoring</p>"...)
+
+	err := smtp.SendMail("SMTP_SERVER:PORT", authentication, "FROM_ADDRESS", to, msg)
+	if err != nil {
+		log.Fatalf("%s - Failed to send email", err)
+	}
+	fmt.Print("email sent")
+}
+
 func main() {
 	var hosts []string
 	var status []string
+	var offlineHosts []string
 	receive := make(chan string)
 
 	hostsfile, err := os.Open("hosts.csv")
@@ -67,7 +94,14 @@ func main() {
 	}
 
 	for i := range status {
-		fmt.Println(status[i])
+		if strings.Contains(status[i], "Offline") {
+			offlineHosts = append(offlineHosts, status[i])
+		} else {
+			continue
+		}
 	}
 
+	if len(offlineHosts) >= 1 {
+		emailSend(offlineHosts)
+	}
 }
